20c3a866765130c39cd2b8d0182fa3845a31fb1a
kennethbchen <kennethbchen@gmail.com>
2022-03-08 19:26:58 -0500

    Updated compile and TargetSDKVersion (30->31); Added PlaidLinkHandler.js

---
+++ plaid_components/PlaidLinkHandler.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+
+import {Text} from 'react-native';
+
+import { PlaidLink, LinkSuccess, LinkExit } from 'react-native-plaid-link-sdk';
+
+// https://birdboombox.com/
+const urlBase = "http://192.168.1.217:8000/";
+
+// This component handles the PlaidLink component from react-native-plaid-link-sdk
+// The PlaidLink Component requires a link token in order to work properly
+// When this component is loaded, it asynchronously requests a link token from the backend server.
+// When the link token is not yet retrieved, the component displays a loading screen and waits for the link token
+// When the link token is retrieved, the component uses that to render the PlaidLink Component which allows user login
+export default class PlaidLinkHandler extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {linkToken: new String()};
+    }
+    
+    async componentDidMount() {
+        // Get Link Token asynchronously when this component is displayed
+        const response = await fetch(urlBase + "api/create_link_token", {
+            method: 'POST'
+        });
+        
+        // When the link token is retrieved, then save that into the component state
+        const data = await response.json();
+        this.setState({linkToken: data.link_token})
+    }
+
+    render() {
+        if (this.state.linkToken == '') {
+            // No link token yet, display loading screen
+            return <Text>Loading...</Text>
+        } else {
+            // Use the link token in the PlaidLink Component
+            return (
+                <PlaidLink
+                    tokenConfig={{
+                        token: this.state.linkToken,
+                    }}
+                    onSuccess={(success) => {
+                    console.log(success);
+                    }}
+                    onExit={(exit) => {
+                    console.log(exit);
+                    }}
+                >
+                
+                <Text>Add Account</Text>
+                </PlaidLink>
+            )
+
+        }
+
+            
+      
+    }
+  }
