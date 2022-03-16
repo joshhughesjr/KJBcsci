b145fe161eb2412d4f513a36f59481c84a58de08
BradyCR49 <70081844+BradyCR49@users.noreply.github.com>
2022-03-16 15:45:34 -0400

    New Calendar added ready for API data

--- screens/Calenderscreen.js
+++ screens/Calenderscreen.js
@@ -1,36 +1,124 @@
-import React, {Component} from 'react';
+import React, {Component, useState } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import AppRegistry from 'react-native';
 import moment from 'moment';
-import CalendarStrip from 'react-native-calendar-strip';
-
+import {Agenda, Calendar, CalendarList} from 'react-native-calendars';
+import type Moment from 'moment';
 const width = Dimensions.get('window').width;
-const CalenderScreen = () => {
+let fetchedDates = ["2022-03-15", "2022-03-16", "2022-03-17", "2022-03-18"];
+let markedDatesArray = [];
+
+/*for (let i = 0; i < fetchedDates.length; i++) {
+  markedDatesArray.push({
+    date: moment(`${fetchedDates[i]}`, "YYYY-MM-DD"),
+    dots: [
+      {
+        color: '#000000',
+      },
+    ],
+  });
+}
+*/
+
+export default class CalenderScreen extends Component<State> {
+  state: State = {
+    items: undefined
+  };
+  render() {
   return (
-    <View style={styles.center}>
-      <CalendarStrip
-        scrollable
-        calendarAnimation={{type: 'sequence', duration: 30}}
-        daySelectionAnimation={{type: 'border', duration: 200, borderWidth: 1, borderHighlightColor: 'white'}}
-        style={{height: 100, paddingTop: 20, paddingBottom: 10}}
-        calendarHeaderStyle={{color: 'white'}}
-        calendarColor={'#6ebf4a'}
-        dateNumberStyle={{color: 'white'}}
-        dateNameStyle={{color: 'white'}}
-        highlightDateNumberStyle={{color: 'yellow'}}
-        highlightDateNameStyle={{color: 'yellow'}}
-        disabledDateNameStyle={{color: 'grey'}}
-        disabledDateNumberStyle={{color: 'grey'}}
-        iconContainer={{flex: 0.1}}
-                />
-    </View>
+        <Agenda 
+        selected={moment().format("YYYY-MM-DD")}
+        items={this.state.items}
+        loadItemsForMonth={this.loadItems}
+        selected={'2022-03-16'}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+        rowHasChanged={this.rowHasChanged}
+        showClosingKnob={true}
+        theme={{
+        backgroundColor: '#ffffff',
+        calendarBackground: '#6ebf4a',
+        textSectionTitleColor: 'white',
+        textSectionTitleDisabledColor: 'black',
+        selectedDayBackgroundColor: 'yellow',
+        selectedDayTextColor: 'grey',
+        todayTextColor: 'black',
+        dayTextColor: 'black',
+        textDisabledColor: 'grey',
+        dotColor: 'yellow',
+        selectedDotColor: '#ffffff',
+        arrowColor: 'black',
+        disabledArrowColor: 'grey',
+        monthTextColor: '#6ebf4a',
+        indicatorColor: '#6ebf4a'
+        }}
+        />
   );
 };
 
-const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-  },
-});
+loadItems = (day: DateData) => {
+    const items = this.state.items || {};
 
-export default CalenderScreen;
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = this.timeToString(time);
+
+        if (!items[strTime]) {
+          items[strTime] = [];
+          
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Item for ' + strTime + ' #' + j,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+              day: strTime
+            });
+          }
+        }
+      }
+      
+      const newItems: AgendaSchedule = {};
+      Object.keys(items).forEach(key => {
+        newItems[key] = items[key];
+      });
+      this.setState({
+        items: newItems
+      });
+    }, 1000);
+  }
+
+
+  renderEmptyDate = () => {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is empty date!</Text>
+      </View>
+    );
+  }
+
+  rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
+    return r1.name !== r2.name;
+  }
+
+  timeToString(time: number) {
+    const date = new Date(time);
+    return date.toISOString().split('T')[0];
+  }
+}
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: '#ffffff',
+    flex: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+    marginTop: 17
+  },
+  emptyDate: {
+    height: 15,
+    flex: 1,
+    paddingTop: 30
+  }
+});
--- screens/Statisticsscreen.js
+++ screens/Statisticsscreen.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
+import { View, Button, Text, StyleSheet, Dimensions } from "react-native";
+import { ProgressChart } from "react-native-chart-kit";
+const screenWidth = Dimensions.get("window").width;
+
+const data = {
+  labels: ["Spent", "Rem", "Save"], // optional
+  data: [0.7, 0.2, 0.1]
+};
+
+const chartConfig = {
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientToOpacity: 0,
+  color: (opacity = 1) => `rgba(49, 173, 47, ${opacity})`,
+  strokeWidth: 3, // optional, default 3
+  barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
+
 const StatisticsScreen = () => {
   return (
     <View style={styles.center}>
-      <Text>This is the stats screen</Text>
-      <Button title="Go to Details Screen" />
+    <ProgressChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        strokeWidth={16}
+        radius={32}
+        chartConfig={chartConfig}
+        hideLegend={false}
+    />
+      <Text>This is the Statistics screen</Text>
     </View>
   );
 };
