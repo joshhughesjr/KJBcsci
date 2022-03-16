1ac4f31ae55b438660ce10aff37601951280cc84
joshhughesjr <69821677+joshhughesjr@users.noreply.github.com>
2022-03-16 15:47:46 -0400

    Update Goalsscreen.js
    top tabs have been added

--- screens/Goalsscreen.js
+++ screens/Goalsscreen.js
@@ -1,23 +1,50 @@
 import React from "react";
-import { View, Button, Text, StyleSheet, Dimensions } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-
-const GoalsScreen = () => {
+function GoalsScreen() {
   return (
-    <View style={styles.center}>
-      <Text>This is the Goals screen</Text>
+    
+      <TouchableOpacity style={styles.buttonContainer}
+              >
+                <Text>Add Goal</Text>
+              </TouchableOpacity>
+  );
+}
+
+function PlansScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Plans will be here</Text>
     </View>
   );
-};
+}
+
+const Tab = createMaterialTopTabNavigator();
+
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
+
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#6ebf4a",
   },
 });
 
-export default GoalsScreen;
+export default function App() {
+  return (
+   
+      <Tab.Navigator>
+        <Tab.Screen name="Goals" component={GoalsScreen} />
+        <Tab.Screen name="Plans" component={PlansScreen} />
+      </Tab.Navigator>
+
+  );
+}
