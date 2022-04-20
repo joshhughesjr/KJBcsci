f7918f19055a5ca3bf66237f7d0b903b41ad9b2b
kennethbchen <kennethbchen@gmail.com>
2022-04-20 14:58:44 -0400

    Styled Add Goal Button

--- screens/Goalsscreen.js
+++ screens/Goalsscreen.js
@@ -2,33 +2,16 @@ import React from "react";
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-function GoalsScreen() {
-  return (
-    
-      <TouchableOpacity style={styles.buttonContainer}
-              >
-                <Text>Add Goal</Text>
-              </TouchableOpacity>
-  );
-}
-
-function PlansScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Plans will be here</Text>
-    </View>
-  );
-}
-
-const Tab = createMaterialTopTabNavigator();
-
-
 const styles = StyleSheet.create({
-
+  center: {
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
   buttonContainer: {
     marginTop:10,
     height:45,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom:20,
@@ -37,6 +20,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#6ebf4a",
   },
 });
+
+var goals = [
+  {
+    goal_date: new Date(),
+    save_goal: 2000,
+    amount_saved: 200
+  }
+]
+
+function GoalsScreen() {
+  return (
+      <View style={styles.center}>
+      <TouchableOpacity style={styles.buttonContainer}><Text>Add Goal</Text></TouchableOpacity>
+      </View>
+  );
+}
+
+function PlansScreen() {
+  return (
+    <View style={styles.center}>
+      <Text>Plans will be here</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
